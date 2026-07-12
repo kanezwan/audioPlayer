5cc03fc4b591031cadd3418275a9bf528512ac2c
kanesun <spkettas@gmail.com>
2026-07-12 15:05:56 +0800

    feat: add FileScanner service

---
+++ audioPlayer/Services/FileScanner.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct FileScanner {
+    static let supportedExtensions = ["wav", "mp3", "aac"]
+
+    func scanFolder(at url: URL) throws -> [AudioFileItem] {
+        let contents = try FileManager.default.contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: [.isRegularFileKey],
+            options: [.skipsHiddenFiles]
+        )
+
+        return contents
+            .filter { url in
+                let ext = url.pathExtension.lowercased()
+                return Self.supportedExtensions.contains(ext)
+            }
+            .map { AudioFileItem(url: $0, name: $0.lastPathComponent) }
+            .sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
+    }
+}
